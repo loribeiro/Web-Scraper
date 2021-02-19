2c20e143b6cf8cd4be3012c6902efe1169d41e9d
Lucas Ribeiro <l.oribeiro@outlook.com>
2021-02-19 13:13:28 -0300

    removing unecessary selectors

--- crawler_tjal/html_selectors/tjalSelectors.js
+++ crawler_tjal/html_selectors/tjalSelectors.js
@@ -1,21 +1,5 @@
-const classeSelector = `body > div > table:nth-child(4) > tbody > tr > td > div:nth-child(7) > 
-    table.secaoFormBody > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr > td >
-     span:nth-child(1) > span`
 
-const areaSelector =  `body > div > table:nth-child(4) > tbody > tr > td > div:nth-child(7) > 
-    table.secaoFormBody > tbody > tr:nth-child(3) > td:nth-child(2) > table > tbody > tr > td`
-
-const assuntoSelector = `body > div > table:nth-child(4) > tbody > tr > td > div:nth-child(7) > 
-    table.secaoFormBody > tbody > tr:nth-child(4) > td:nth-child(2) > span`
-
-const dataDistribuicaoSelector = `body > div > table:nth-child(4) > tbody > tr > td > div:nth-child(7) > 
-    table.secaoFormBody > tbody > tr:nth-child(6) > td:nth-child(2) > span`
-
-const juizSelector = `body > div > table:nth-child(4) > tbody > tr > td > div:nth-child(7) > table.secaoFormBody > tbody > 
-    tr:nth-child(9) > td:nth-child(2) > span`
-
-const valorAcaoSelector = `body > div > table:nth-child(4) > tbody > tr > td > div:nth-child(7) > table.secaoFormBody > tbody > 
-    tr:nth-child(10) > td:nth-child(2) > span`
+const dadosProcessoSeletor =  `body > div > table:nth-child(4) > tbody > tr > td > div:nth-child(7) > table.secaoFormBody`
 
 const partesProcessoSelector = `#tablePartesPrincipais`
 
@@ -23,12 +7,7 @@ const movimentacoesSelector = `#tabelaTodasMovimentacoes`
 
 
 module.exports = {
-    classeSelector,
-    areaSelector,
-    assuntoSelector,
-    dataDistribuicaoSelector,
-    juizSelector,
-    valorAcaoSelector,
+    dadosProcessoSeletor,
     partesProcessoSelector,
     movimentacoesSelector
 }
