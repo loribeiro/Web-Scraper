3aa6aa7cf4ee49d8395fabdcd39fb813b62cb647
Lucas Ribeiro <l.oribeiro@outlook.com>
2021-02-19 14:33:03 -0300

    cleaning up unnessary code

--- crawler_tjal/retrieveInformationHtml.js
+++ crawler_tjal/retrieveInformationHtml.js
@@ -2,42 +2,12 @@ const cheerio = require("cheerio")
 const seletoresInstancia1 = require("./html_selectors/tjalSelectors")
 const seletoresInstancia2 = require("./html_selectors/tjalSelectorsSecondInstance")
 
-
 function retrieveInformation(document, instance=1){
     const $ = cheerio.load(document) //jquery notation
-
-    function __getClasse(){
-        const seletor = instance === 1 ? seletoresInstancia1 : seletoresInstancia2
-        return $("body").find(seletor.classeSelector).text().trim()
-    }
-
-    function __getArea(){
-        const seletor = instance === 1 ? seletoresInstancia1 : seletoresInstancia2
-
-        return $("body").find(seletor.areaSelector).text().trim()
-    }
-
-    function __getAssunto(){
-        const seletor = instance === 1 ? seletoresInstancia1 : seletoresInstancia2
-
-        return $("body").find(seletor.assuntoSelector).text().trim() 
-    }
-
-    function __getDistribuicao(){
-        return $("body").find(seletoresInstancia1.dataDistribuicaoSelector).text().trim()
-    }
-
-    function __getJuiz(){
-        return $("body").find(seletoresInstancia1.juizSelector).text().trim()
-    }
-
-    function __getValorAcao(){
-        return $("body").find(seletoresInstancia1.valorAcaoSelector).text().trim()
-    }
-
+ 
     function __getPartesProcesso(){
-        const info = $("body").find(seletoresInstancia1.partesProcessoSelector).text().trim()
-            .replace(/\t/g,'').split("\n").filter(item => /\S/.test(item))
+        const element = $("body").find(seletoresInstancia1.partesProcessoSelector)
+        const info = cleanElement(element, $)
 
         const object = info.reduce((acumulator, currentValue, index)=>{
             if(index %2 === 0){
@@ -53,45 +23,89 @@ function retrieveInformation(document, instance=1){
         const seletor = instance === 1 ? seletoresInstancia1 : seletoresInstancia2
         const movimentacoes = []
         const info = $("body").find(seletor.movimentacoesSelector).find("tr").each((index, element)=>{
-             const treatedAsArray = $(element).text().replace(/\t/g,'').trim() //replaces all occurrences of "\t" with "" 
-                .split("\n")  // transform into array by spliting the string on "\n"
-                .filter(item => /\S/.test(item)) // removes all the occurences of "" character
-
-             const data = treatedAsArray.shift()
-              
-             movimentacoes.push({
-                 "data": data,
-                 "movimentação": treatedAsArray.join(" \n")
-             })
+            const treatedAsArray = cleanElement(element, $)
+            const data = treatedAsArray.shift()
+            
+            movimentacoes.push({
+                "data": data,
+                "movimentação": treatedAsArray.join(" \n")
+            })
 
         })
         
         return movimentacoes
     }
 
-    function execute(){
-        if(instance === 1){
+    function __getDadosProcesso(){
+        const seletor = instance === 1 ? seletoresInstancia1 : seletoresInstancia2
+        const dadosProcesso = {}
+        $("body").find(seletor.dadosProcessoSeletor).find("tr").each((index, element)=>{
+            const treatedAsArray = cleanElement(element, $)
+            switch(treatedAsArray[0]){
+                case "Classe:":
+                    dadosProcesso["classe"] = treatedAsArray[1]
+                    break
+                case "Assunto:":
+                    dadosProcesso["assunto"] = treatedAsArray[1]
+                    break
+                case "Área :":
+                    dadosProcesso["area"] = treatedAsArray[1]
+                case "Distribuição:":
+                    dadosProcesso["data"] = treatedAsArray[1]
+                    break
+                case "Juiz:":
+                    dadosProcesso["juiz"] = treatedAsArray[1]
+                    break
+                case "Valor da ação:":
+                    dadosProcesso["valor"] = treatedAsArray[1]
+                    break
+                default:
+                    if(treatedAsArray[0].startsWith("Área:")){
+                        dadosProcesso["area"] = treatedAsArray[0].replace('Área:','')
+                        break
+                    }else{
+                        break
+                    }
+            }
+            
+        })
+       
+        return dadosProcesso
+    }
 
+    function execute(){
+        const dadosProcesso = __getDadosProcesso() 
+        if(instance === 1){
             return {
-                "classe": __getClasse(),
-                "área": __getArea(),
-                "assunto": __getAssunto(),
-                "data de distribuição": __getDistribuicao(),
-                "juiz": __getJuiz(),
-                "valor da ação": __getValorAcao(),
+                "classe": dadosProcesso.classe,
+                "área": dadosProcesso.area,
+                "assunto": dadosProcesso.assunto,
+                "data de distribuição": dadosProcesso.data,
+                "juiz": dadosProcesso.juiz,
+                "valor da ação": dadosProcesso.valor,
                 "partes do processo": __getPartesProcesso(),
                 "lista das movimentações (data e movimento)": __getListaMovimentacoes(),
             }
         }else{
+           
             return {
-                "classe": __getClasse(),
-                "área": __getArea(),
-                "assunto": __getAssunto(),
+                "classe": dadosProcesso.classe,
+                "área": dadosProcesso.area,
+                "assunto": dadosProcesso.assunto,
                 "lista das movimentações (data e movimento)": __getListaMovimentacoes(),
             }
         }
     }
     return execute()
 }
+
 module.exports = retrieveInformation;
 
+
+function cleanElement(element,$){
+    const treatedAsArray = $(element).text().replace(/\t/g,'').trim() //replaces all occurrences of "\t" with "" 
+    .split("\n")  // transform into array by spliting the string on "\n"
+    .filter(item => /\S/.test(item)) // removes all the occurences of "" character
+
+    return treatedAsArray
+}
