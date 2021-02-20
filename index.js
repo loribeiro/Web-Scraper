31398f4f8de5bfe4234016f96b0f598c36e737e1
Lucas Ribeiro <l.oribeiro@outlook.com>
2021-02-20 17:53:49 -0300

    removing complexity from server presentation

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
-const crawlTJAL = require("./crawler_tjal/crawlTjal")
-const crawlTJMS = require("./crawler_tjms/crawlTjms")
-
+const responseController = require("./controller") 
 const http = require("http")
 
 const port = process.env.PORT || 3000
 
 const requestListener = function (request, response) {
     if (request.method !== 'POST') {
+        response.writeHead(400,{'Content-Type': 'application/json'});
         response.end(`{"error": "${http.STATUS_CODES[405]}"}`)
     } else {
-        response.writeHead(200);
+        
         request.on("data",async (chunk)=>{
 
             const input = await JSON.parse(chunk.toString())
-            const code = await input.code.toString()
-            
-            if(await code.includes(".8.12.")){
-                crawlTJMS(code).then(resp=>{
-                    response.end(JSON.stringify(resp))
-                }).catch(err =>{
-                    console.log(err)
-                    response.end(`{"error": "${http.STATUS_CODES[404]}"}`)      
-                })
-            }else if(await code.includes(".8.02.")){
-                crawlTJAL(code).then(resp=>{
-                    response.end(JSON.stringify(resp))
-                }).catch(err => {
-                    console.log(err)
-                    response.end(`{"error": "${http.STATUS_CODES[404]}"}`)
-
-                })
-            }else{
-                response.end(`{"error": "${http.STATUS_CODES[404]}"}`)
-            }
+            const code = input.code
+            responseController(code, response)
         })
+
         request.on("error",()=>response.end(`{"error": "${http.STATUS_CODES[404]}"}`))
         
     }
