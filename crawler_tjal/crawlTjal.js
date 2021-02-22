57277197067631a3f57d0e79b3ee5b84d8d331b3
Lucas Ribeiro <l.oribeiro@outlook.com>
2021-02-21 22:19:23 -0300

    improving code readability

--- crawler_tjal/crawlTjal.js
+++ crawler_tjal/crawlTjal.js
@@ -5,63 +5,63 @@ const Nightmare =  require("nightmare")
 function crawlTJAL(code){
     const ERR_NAME_NOT_RESOLVED = -105
     const ERR_CONTENT_NOT_LOADED = -7
+    let splitedCode = []
+    const pattern  = /^\d{7}-\d{2}.\d{4}.\d{1}.\d{2}.\d{4}$/ // regex to ensure code correct pattern NNNNNNN-DD.AAAA.J.TR.OOOO
 
-    async function executeRequisition(url, firstPartCode, secondPartCode, instance, tentatives = 1){
+    async function executeRequisition(url, instance, tentatives = 1){
         return await new Promise(async function(resolve, reject){
             const nightmare = Nightmare({show: false, waitTimeout: 10000, gotoTimeout:5000})
+            const [firstPartCode,secondPartCode] = splitedCode
+
             await nightmare
                      .goto(url)
-                     .wait("#linhaProcessoUnificado")
-                     .insert("#numeroDigitoAnoUnificado", firstPartCode)
-                     .insert("#foroNumeroUnificado", secondPartCode)
-                     .click(instance === 1 ? "#pbEnviar" : "#botaoPesquisar")
+                     .wait("#linhaProcessoUnificado") // waits until input area is visible 
+                     .insert("#numeroDigitoAnoUnificado", firstPartCode) // inputs the first part of the code
+                     .insert("#foroNumeroUnificado", secondPartCode) // inputs the second part of the code
+                     .click(instance === 1 ? "#pbEnviar" : "#botaoPesquisar") // clicks on button to search for the process
                      .wait(1000)
-                     .evaluate(() => document.querySelector("body").innerHTML)
+                     .evaluate(() => document.querySelector("body").innerHTML) // retrieves html from loaded pag
                      .end()
                      .then(doc =>{
-                         const chaves = retrieveInformationHtml(doc,instance)
-                         resolve(chaves)
+                         const information = retrieveInformationHtml(doc,instance)
+                         resolve(information) // returns the information scraped from html 
                      })
                      .catch(error => {
                         if(error.code === ERR_NAME_NOT_RESOLVED){
                             resolve({
-                                "erro": "503"
+                                "error": "503"
                             })
-                        }else if(error.code === ERR_CONTENT_NOT_LOADED && tentatives <2){
-                           resolve(executeRequisition(url, firstPartCode, secondPartCode, instance,2))
+                        }else if(error.code === ERR_CONTENT_NOT_LOADED && tentatives < 2){
+                           resolve(executeRequisition(url, instance, 2))
                         }else{
                             resolve({
-                                "erro": "408"
+                                "error": "408"
                             })
                         }
                      })
          })
     }
 
-    async function secondInstance(codeBreaked){
+    async function secondInstance(){
         const url = "https://www2.tjal.jus.br/cposg5/open.do"
-        const firstPartCode = codeBreaked[0]
-        const secondPartCode = codeBreaked[1]
 
-        return await executeRequisition(url, firstPartCode, secondPartCode, 2)
+        return await executeRequisition(url, 2)
     }
 
-    async function firstInstance(codeBreaked){
+    async function firstInstance(){
         const url = "https://www2.tjal.jus.br/cpopg/open.do"
-        const firstPartCode = codeBreaked[0]
-        const secondPartCode = codeBreaked[1]
 
-        return await executeRequisition(url, firstPartCode, secondPartCode, 1)
+        return await executeRequisition(url, 1)
     }
     
     async function execute(){
-        const pattern  = /^\d{7}-\d{2}.\d{4}.\d{1}.\d{2}.\d{4}$/
+       
         if(pattern.test(code)){
-            const codeBreaked = code.split(".8.02.")
+            splitedCode = code.split(".8.02.")
 
             const result = await Promise.all([
-                firstInstance(codeBreaked),
-                secondInstance(codeBreaked)
+                firstInstance(),
+                secondInstance()
             ]).catch(err=>console.log(err))
            
             return {
@@ -70,7 +70,7 @@ function crawlTJAL(code){
             }
         }else{
             return {
-                "erro": "400",
+                "error": "400",
                 "mensagem auxiliar": "codigo no formato errado, deve estar no formato: NNNNNNN-DD.AAAA.J.TR.OOOO"
             }
         } 
--- crawler_tjms/crawlTjms.js
+++ crawler_tjms/crawlTjms.js
@@ -4,64 +4,63 @@ const Nightmare =  require("nightmare")
 function crawlTjms(code){
     const ERR_NAME_NOT_RESOLVED = -105
     const ERR_CONTENT_NOT_LOADED = -7
+    let splitedCode = []
+    const pattern  = /^\d{7}-\d{2}.\d{4}.\d{1}.\d{2}.\d{4}$/ // regex to ensure code correct pattern NNNNNNN-DD.AAAA.J.TR.OOOO
 
-    async function executeRequisition(url, firstPartCode, secondPartCode, instance, tentatives = 1){
+    async function executeRequisition(url, instance, tentatives = 1){
+
         return await new Promise(async function(resolve, reject){
             const nightmare = Nightmare({show: false, waitTimeout: 10000, gotoTimeout:5000})
+            const [firstPartCode,secondPartCode] = splitedCode
             await nightmare
                      .goto(url)
-                     .wait("#numeroDigitoAnoUnificado")
-                     .insert("#numeroDigitoAnoUnificado",firstPartCode)
-                     .insert("#foroNumeroUnificado",secondPartCode)
-                     .click(instance === 1 ? "#botaoConsultarProcessos" : "#pbConsultar")
+                     .wait("#numeroDigitoAnoUnificado") // waits until input area is visible 
+                     .insert("#numeroDigitoAnoUnificado",firstPartCode)  // inputs the first part of the code
+                     .insert("#foroNumeroUnificado",secondPartCode) // inputs the second part of the code
+                     .click(instance === 1 ? "#botaoConsultarProcessos" : "#pbConsultar") // clicks on button to search for the process
                      .wait(1000)
-                     .evaluate(() => document.querySelector("body").innerHTML)
+                     .evaluate(() => document.querySelector("body").innerHTML) // retrieves html from loaded page
                      .end()
                      .then(doc =>{
                          const information = retrieveInformationHtml(doc, instance)
-                         resolve(information)
+                         resolve(information) // returns the information scraped from html 
                      })
                      .catch(error => {
                          //console.log(error.code)
                          if(error.code === ERR_NAME_NOT_RESOLVED){
                              resolve({
-                                 "erro": "503"
+                                 "error": "503"
                              })
-                         }else if(error.code === ERR_CONTENT_NOT_LOADED && tentatives <2){
-                            resolve(executeRequisition(url, firstPartCode, secondPartCode, instance,2))
+                         }else if(error.code === ERR_CONTENT_NOT_LOADED && tentatives < 2){
+                            resolve(executeRequisition(url, instance, 2))
                          }else{
                              resolve({
-                                 "erro": "408"
+                                 "error": "408"
                              })
                          }
                      })
          })
     }
     
-    async function secondInstance(codeBreaked){
+    async function secondInstance(){
         const url = "https://esaj.tjms.jus.br/cposg5/open.do"
-        const firstPartCode = codeBreaked[0]
-        const secondPartCode = codeBreaked[1]
-
-        return await executeRequisition(url, firstPartCode, secondPartCode, 2)
+       
+        return await executeRequisition(url, 2)
     }
 
-    async function firstInstance(codeBreaked){
+    async function firstInstance(){
         const url = "https://esaj.tjms.jus.br/cpopg5/open.do"
-        const firstPartCode = codeBreaked[0]
-        const secondPartCode = codeBreaked[1]
-
-        return await executeRequisition(url, firstPartCode, secondPartCode, 1)
-        
+    
+        return await executeRequisition(url, 1)
     }
     
     async function crawl(){
-        const pattern  = /^\d{7}-\d{2}.\d{4}.\d{1}.\d{2}.\d{4}$/
         if(pattern.test(code)){
-            const codeBreaked = code.split(".8.12.")
+
+            splitedCode = code.split(".8.12.")
             const result = await Promise.all([
-                firstInstance(codeBreaked),
-                secondInstance(codeBreaked)
+                firstInstance(),
+                secondInstance()
             ]).catch(err=>console.log(err))
            
             return {
@@ -71,7 +70,7 @@ function crawlTjms(code){
         }else{
 
             return {
-                "erro": "400",
+                "error": "400",
                 "mensagem auxiliar": "codigo no formato errado, deve estar no formato: NNNNNNN-DD.AAAA.J.TR.OOOO"
             }
         } 
@@ -80,4 +79,3 @@ function crawlTjms(code){
 }
 
 module.exports = crawlTjms;
-
