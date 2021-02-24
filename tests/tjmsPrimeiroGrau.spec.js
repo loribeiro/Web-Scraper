1c838ab6b09487736d24b9aacf4e50daf87fd309
Lucas Ribeiro <l.oribeiro@outlook.com>
2021-02-24 20:44:17 -0300

    adding new test cases

---
+++ tests/tjmsPrimeiroGrau.spec.js
@@ -0,0 +1,16 @@
+const crawlTJMS = require("../crawler_tjms/crawlTjms")
+const fs = require("fs")
+
+describe("TJMS Primeiro Grau", ()=>{
+    const html = fs.readFileSync(__dirname + "/tjms_html/primeiro_grau/pagina_resultado/resultado.htm","utf8")
+    const code = "0821901-51.2018.8.12.0001"
+    const testCrawl = crawlTJMS(code, true)
+
+    test("it should return the result of the first instance", ()=>{
+        const input = testCrawl.retrieveInstance(html, 1)
+        const output = require("./tjms_responses/first_instance/response.json")
+
+        expect(input).toMatchObject(output["primeira instancia"]);
+    })
+    
+})
---
+++ tests/tjmsSegundoGrau.spec.js
@@ -0,0 +1,16 @@
+const crawlTJMS = require("../crawler_tjms/crawlTjms")
+const fs = require("fs")
+
+describe("TJMS Segundo Grau", ()=>{
+    const html = fs.readFileSync(__dirname + "/tjms_html/segundo_grau/pagina_resultado/resultado.htm","utf8")
+    const code = "0821901-51.2018.8.12.0001"
+    const testCrawl = crawlTJMS(code, true)
+
+    test("it should return the result of the first instance", ()=>{
+        const input = testCrawl.retrieveInstance(html, 2)
+        const output = require("./tjms_responses/second_instance/response.json")
+
+        expect(input).toMatchObject(output["segunda instancia"]);
+    })
+    
+})
