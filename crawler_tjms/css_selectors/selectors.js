d9858217334b228dc593cf0bcf08b4d08d8c0b12
Lucas Ribeiro <l.oribeiro@outlook.com>
2021-02-25 11:02:20 -0300

    serparating css selectors

---
+++ crawler_tjms/css_selectors/selectors.js
@@ -0,0 +1,17 @@
+const classeSelector = "#classeProcesso"
+const areaSelector = "#areaProcesso"
+const subjectSelector = "#assuntoProcesso"
+const dateSelector = "#dataHoraDistribuicaoProcesso"
+const judgeSelector = "#juizProcesso"
+const amountSelector = "#valorAcaoProcesso"
+const tableMovimentacoesSelector = "#tabelaUltimasMovimentacoes"
+
+module.exports = {
+    classeSelector,
+    areaSelector,
+    subjectSelector,
+    dateSelector,
+    judgeSelector,
+    amountSelector,
+    tableMovimentacoesSelector
+}
